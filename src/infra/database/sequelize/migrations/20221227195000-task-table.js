c359f12f35caf2f9477adc8fce9a26b94c998172
Kauã Pereira <zkauacodes@hotmail.com>
2022-12-27 17:10:02 -0300

    add: add task-table migration and task-db-model

---
+++ src/infra/database/sequelize/migrations/20221227195000-task-table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.DataTypes.UUID
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.UUID,
+        references: {
+          model: 'accounts',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return await queryInterface.dropTable('tasks')
+  }
+};
--- src/infra/database/sequelize/sequelize-connection.js
+++ src/infra/database/sequelize/sequelize-connection.js
@@ -2,8 +2,20 @@ const Sequelize = require('sequelize')
 const databaseConfig = require('./config/databaseConfig')
 
 const AccountDbModel = require('./models/account-database-model')
+const TaskDbModel = require('./models/task-database-model')
 
 const connection = new Sequelize(databaseConfig)
+
 AccountDbModel.init(connection)
+TaskDbModel.init(connection)
+
+TaskDbModel.associate(connection.models)
+
+// TaskDbModel.create({
+//   id: '2950e933-5162-4e30-b692-ea2f336f833f',
+//   title: 'tarefa 1',
+//   description: 'tarefa 1 desc',
+//   user_id: 'd2cb1657-82d4-4619-b6e7-8748428760a2'
+// })
 
 module.exports = connection
