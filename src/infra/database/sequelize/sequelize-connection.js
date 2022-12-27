d63a05bd2423e53b6b2ef1bb27db4c1f66c6052f
Kauã Pereira <zkauacodes@hotmail.com>
2022-12-27 12:03:09 -0300

    add: add accounts-table migration and AccountDbModel

--- src/infra/database/sequelize/sequelize-connection.js
+++ src/infra/database/sequelize/sequelize-connection.js
@@ -1,6 +1,16 @@
 const Sequelize = require('sequelize')
 const databaseConfig = require('./config/databaseConfig')
 
+const AccountDbModel = require('./models/account-database-model')
+
 const connection = new Sequelize(databaseConfig)
+AccountDbModel.init(connection)
+
+AccountDbModel.create({
+  id: 'a817cf15-765b-487a-a25d-edbf9e022904',
+  name: 'a',
+  email: 'a@hotmail.com',
+  password: 'ab'
+})
 
 module.exports = connection
