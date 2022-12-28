83c2a7c31f7a113de45518a09c659fcaa6f01845
Kauã Pereira <zkauacodes@hotmail.com>
2022-12-28 17:30:55 -0300

    add: add load-tasks-by-user-id-controller in presentation layer

---
+++ src/presentation/controllers/load-tasks-by-user-id-controller.js
@@ -0,0 +1,23 @@
+const { MissingParamError } = require("../../utils/errors")
+const HttpResponse = require("../helpers/http-response")
+
+module.exports = class LoadTasksByUserIdController {
+  constructor({ loadTasksByUserIdUseCase } = {}) {
+    this.loadTasksByUserIdUseCase = loadTasksByUserIdUseCase
+  }
+
+  async handle(httpRequest) {
+    try {
+      const { userId } = httpRequest.params
+      if (!userId) {
+        return HttpResponse.badRequest(new MissingParamError('user_id'))
+      }
+
+      const tasks = await this.loadTasksByUserIdUseCase.loadAll(userId)
+
+      return HttpResponse.ok({ tasks })
+    } catch (error) {
+      return HttpResponse.serverError()
+    }
+  }
+}
