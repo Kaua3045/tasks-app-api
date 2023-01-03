e19c4f609ad9d79316f955ecb34bcbb93f752a71
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-03 20:27:22 -0300

    add: add withoutPassword in constructor to verify return password

--- src/infra/repositories/account/load-account-by-email-db-repository.js
+++ src/infra/repositories/account/load-account-by-email-db-repository.js
@@ -1,8 +1,17 @@
 const AccountDbModel = require("../../database/sequelize/models/account-database-model")
 
 module.exports = class LoadAccountByEmailDbRepository {
+  constructor(withoutPassword) {
+    this.withoutPassword = withoutPassword
+  }
+
   async load(email) {
-    const account = await AccountDbModel.scope('withoutPassword').findOne({ where: { email: email }})
+    if (this.withoutPassword) {
+      const account = await AccountDbModel.scope('withoutPassword').findOne({ where: { email: email }})
+      return account
+    }
+
+    const account = await AccountDbModel.findOne({ where: { email: email }})
     return account
   }
 }
