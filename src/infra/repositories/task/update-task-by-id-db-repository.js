56bc77266c3e0c21b85af40ce31a70c65ada763a
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-03 20:27:43 -0300

    add: add returning and get data

--- src/infra/repositories/task/update-task-by-id-db-repository.js
+++ src/infra/repositories/task/update-task-by-id-db-repository.js
@@ -2,7 +2,7 @@ const TaskDbModel = require("../../database/sequelize/models/task-database-model
 
 module.exports = class UpdateTaskByIdDbRepository {
   async update(id, title, description, completed) {
-    const task = await TaskDbModel.update({ title, description, completed }, { where: { id } })
-    return task
+    const task = await TaskDbModel.update({ title, description, completed }, { where: { id }, plain: true, returning: true })
+    return task[1]
   }
 }
