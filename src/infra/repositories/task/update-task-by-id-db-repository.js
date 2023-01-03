f2d6346a1faca48b333f6bfab0926e8009b5df0f
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-03 19:54:36 -0300

    add: add update-task-by-id-db-repository

---
+++ src/infra/repositories/task/update-task-by-id-db-repository.js
@@ -0,0 +1,8 @@
+const TaskDbModel = require("../../database/sequelize/models/task-database-model")
+
+module.exports = class UpdateTaskByIdDbRepository {
+  async update(id, title, description, completed) {
+    const task = await TaskDbModel.update({ title, description, completed }, { where: { id } })
+    return task
+  }
+}
