4db7da6dacd5432ee7f935eb5ebbda1cdd4accc1
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-05 15:28:45 -0300

    add: delete route for task

--- src/main/routes/task-router.js
+++ src/main/routes/task-router.js
@@ -7,6 +7,7 @@ const { makeAddTaskController } = require('../factory/controllers/task/add-task-
 const { makeLoadTaskByIdController } = require('../factory/controllers/task/load-task-by-id-controller-impl')
 const { makeLoadTasksByUserIdController } = require('../factory/controllers/task/load-tasks-by-user-id-controller-impl')
 const { makeUpdateTaskByIdController } = require('../factory/controllers/task/update-task-by-id-controller-impl')
+const { makeDeleteTaskController } = require('../factory/controllers/task/delete-task-controller-impl')
 
 const auth = require('../middlewares/auth')
 
@@ -14,5 +15,6 @@ taskRouter.get('/:id', auth, adapt(makeLoadTaskByIdController()))
 taskRouter.get('/all/:userId', auth, adapt(makeLoadTasksByUserIdController()))
 taskRouter.post('/create', auth, adapt(makeAddTaskController()))
 taskRouter.patch('/update/:id', auth, adapt(makeUpdateTaskByIdController()))
+taskRouter.delete('/delete/:id', auth, adapt(makeDeleteTaskController()));
 
 module.exports = taskRouter
