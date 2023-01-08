b20e87401ea89c06eb210a8e8522ad342b12a907
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-08 08:39:15 -0300

    delete: old tasks repositories

--- src/infra/repositories/task/delete-task-db-repository.js
+++
@@ -1,7 +0,0 @@
-const TaskDbModel = require("../../database/sequelize/models/task-database-model")
-
-module.exports = class DeleteTaskDbRepository {
-  async remove(id) {
-    await TaskDbModel.destroy({ where: { id }})
-  }
-}
