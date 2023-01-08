4667637dfe829eac835aa4f1380768bfcf518f50
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-08 08:31:30 -0300

    delete: old accounts repositories

--- src/infra/repositories/account/confirm-account-db-repository.js
+++
@@ -1,9 +0,0 @@
-const AccountDbModel = require('../../database/sequelize/models/account-database-model')
-
-module.exports = class confirmAccountDbRepository {
-  async update(id, confirmed) {
-    const account = await AccountDbModel
-      .update({ confirm: confirmed }, { where: { id }, returning: true })
-    return account[1]
-  }
-}
