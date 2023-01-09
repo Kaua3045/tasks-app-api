b3e20b0a7aaf0de32547c00a8b1b4ca3b58bdcff
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-09 20:55:40 -0300

    feat: add rate-limit config

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -3,11 +3,13 @@ const express = require('express')
 const { serve, setup } = require('swagger-ui-express')
 
 const swaggerConfig = require('../docs')
+const rateLimit = require('./rate-limit')
 
 const app = express()
 
 require('../../infra/database/sequelize/sequelize-connection')
 
+app.use(rateLimit)
 app.use('/api-docs', serve, setup(swaggerConfig))
 app.use(express.json())
 app.use('/api', require('../routes/routes'))
